06d77ee32399b26ecd0d412bf8aa2e884daf66ef
Viktoriia <kharvi1826@gmail.com>
2022-06-30 23:49:21 +0200

    Добавила цикл

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Поиск случайного целого числа//
 const getRandomInt = (min, max) => {
-  if (min >= 0 && max >= 0){
+  if (min >= 0 && max >= 0) {
     if (min === max) {
       return max;
     }
@@ -18,46 +18,47 @@ checkStringLength(20);
 //Входные данные//
 const DESCRIPTIONS = ['Кот', 'Бабочка', 'Снежинка', 'Автомобиль', 'Колокольчик', 'Мороженое'];
 const MESSAGES = ['Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В целом всё неплохо. Но не всё.'];
-const NAMES = ['Римма', 'Света', 'Федор', 'Галя', 'Ваня', 'Илья', 'Олег', 'Коля' , 'Аля'];
+const NAMES = ['Римма', 'Света', 'Федор', 'Галя', 'Ваня', 'Илья', 'Олег', 'Коля', 'Аля'];
 const itemsCount = 25;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
 const MIN_ID_AVATAR = 1;
 const MAX_ID_AVATAR = 6;
 
-//Проверяем на уникальность каждый ID//
-const usedIndexes = new Set();
-const getUniqueRandomNumber = (callback) => {
-  const newNumber = callback;
-  if (usedIndexes.has(newNumber)) {
-    return usedIndexes.getUniqueRandomNumber(callback);
-  }
-  usedIndexes.add(newNumber);
-  return newNumber;
-};
-
 //Создаём массив случайных элементов//
 const getRandomElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
+
+
+const galleryPhotos = [];
 
 //Генерируем случайный комментарий//
 const getComment = () => (
   {
-    id: getUniqueRandomNumber(getRandomInt),
-    avatar: 'img/avatar-{getRandomInt(MIN_ID_AVATAR, MAX_ID_AVATAR)}.svg',
+    id: getRandomInt(MIN_ID_AVATAR, MAX_ID_AVATAR),
+    avatar: `img/avatar-${getRandomInt(MIN_ID_AVATAR, MAX_ID_AVATAR)}.svg`,
     message: getRandomElement(MESSAGES),
     name: getRandomElement(NAMES),
   });
 
-//Добавляем фото с описанием//
-  const getPhoto = (index) =>  ({
-  id: index,
-  url: 'photos/{i}.jpg',
-  description: getRandomElement(DESCRIPTIONS),
-  likes: getRandomInt(MIN_LIKES, MAX_LIKES),
-  comments: Array.from({length: getRandomInt(1, 10)}, getComment),
-});
+  //Добавляем фото с описанием//
+const getPhoto = (i) => {
+  return {
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: getRandomElement(DESCRIPTIONS),
+    likes: getRandomInt(MIN_LIKES, MAX_LIKES),
+    comments: Array.from({ length: getRandomInt(1, 10) }, getComment),
+}
+};
 
-//Заполняем массив//
-const gallery = new Array(itemsCount).fill(null).map((index) => getPhoto(index + 1));
+  for (let i = 1; i <= itemsCount; i++) {
+    const photo = getPhoto(i);
+    galleryPhotos.push(photo);
+  }
 
-gallery;
+  const photos = Array.from({length: 25}, (_, i) => getPhoto(i))
+  console.log(photos);
+
+
+
+
